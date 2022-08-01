ebb866db5ad8361323c17a7a70b668d3c3bb1342
SylvainLeBourrin <sylvain.veirman@3wa.io>
2022-08-01 16:24:37 +0200

    AfterSensei
    après passage du prof avec une tentative de mal de crane échoué.

--- Assets/Players/Player.cs
+++ Assets/Players/Player.cs
@@ -8,29 +8,50 @@ public class Player : MonoBehaviour
     [SerializeField] float _speed = 8f;
 
     public Rigidbody _rigidbody;
-
+    private Vector3 _mouvementInput;
     //private bool isVerticalPressed = false;
-    
+    private void FixedUpdate()
+    {
+        Vector3 velocity;
+        velocity = (_mouvementInput * _speed);
+       _rigidbody.velocity = velocity;
+         
+    }
 
     // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Start()
+    {
+       
         
 
     }
     // Update is called once per frame
+   
     void Update()
     {
-        float verticalInput = Input.GetAxis("Vertical");
-        float horizontalInput = Input.GetAxis("Horizontal");
+        float horizontal =  Input.GetAxisRaw("Horizontal");
+        float vertical   =  Input.GetAxisRaw("Vertical");
+        float orientationHorizontal = Input.
+        //
+        _mouvementInput = new Vector3(horizontal, 0, vertical); 
+        //Vector3.Normalize("velocity");  //= ("horizontal", 0, "vertical");
 
-        Debug.Log(verticalInput);
+
+
+        // float verticalInput = Input.GetAxis("Vertical");
+        //float horizontalInput = Input.GetAxis("Horizontal");
+
+        // Debug.Log(verticalInput);
 
         //isVerticalPressed = Input.GetButtonDown("Vertical");
-          //  if (isVerticalPressed)
-       // {
-            _rigidbody.velocity = new Vector3( verticalInput, 0,  horizontalInput) * _speed;
+        //  if (isVerticalPressed)
+        // {
+        //   _rigidbody.velocity = new Vector3( verticalInput, 0,  horizontalInput);
         //}
     }
 
